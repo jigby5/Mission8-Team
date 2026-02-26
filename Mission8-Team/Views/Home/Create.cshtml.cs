7554ae33032d37360e3eef1b5246f6362f12cbbc
bfpead1 <blake.pead@gmail.com>
2026-02-26 16:49:43 -0700

    Added some code for the websites. Can't finsih without the databse

---
+++ Mission8-Team/Views/Home/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mission8_Team.Views.Home
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Mission8-Team/Views/Home/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mission8_Team.Views.Home
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
