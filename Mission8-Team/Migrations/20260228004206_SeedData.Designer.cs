b2c2b3b15b5fab8593bee48d7c4f40ac38108d2b
carsonian07 <carson07williams@gmail.com>
2026-02-27 19:56:24 -0700

    Step 3: Quadrants view + edit/delete

--- Mission8-Team/Migrations/20260228004206_SeedData.Designer.cs
+++ Mission8-Team/Migrations/20260228004206_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mission8_Team.Models;
 
@@ -10,9 +11,11 @@ using Mission8_Team.Models;
 namespace Mission8_Team.Migrations
 {
     [DbContext(typeof(TasksContext))]
-    partial class TasksContextModelSnapshot : ModelSnapshot
+    [Migration("20260228004206_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
@@ -66,7 +69,7 @@ namespace Mission8_Team.Migrations
                     b.Property<bool>("Completed")
                         .HasColumnType("INTEGER");
 
-                    b.Property<DateTime?>("DueDate")
+                    b.Property<DateTime>("DueDate")
                         .HasColumnType("TEXT");
 
                     b.Property<int>("Quadrant")
--- Mission8-Team/Models/TasksContext.cs
+++ Mission8-Team/Models/TasksContext.cs
@@ -12,11 +12,19 @@ public class TasksContext : DbContext
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<Category>().HasData(
             new Category { CategoryId = 1, CategoryName = "Home" },
             new Category { CategoryId = 2, CategoryName = "School" },
             new Category { CategoryId = 3, CategoryName = "Work" },
             new Category { CategoryId = 4, CategoryName = "Church" }
+        );
+
+        modelBuilder.Entity<TaskInstance>().HasData(
+            new TaskInstance { TaskId = 1, TaskName = "Finish homework", Quadrant = 1, CategoryId = 2, Completed = false, DueDate = DateTime.Today.AddDays(1) },
+            new TaskInstance { TaskId = 2, TaskName = "Plan week", Quadrant = 2, CategoryId = 2, Completed = false, DueDate = DateTime.Today.AddDays(7) },
+            new TaskInstance { TaskId = 3, TaskName = "Email back", Quadrant = 3, CategoryId = 3, Completed = false, DueDate = DateTime.Today.AddDays(2) }
         );
     }
 }
